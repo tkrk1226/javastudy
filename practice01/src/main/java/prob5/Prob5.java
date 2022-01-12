576798e1c7c7ce5f2491bca4ae8cbe8d19717372
UijinJung <tkrk1226@ajou.ac.kr>
2022-01-12 20:58:46 +0900

    practice01 finish

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,32 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+
+		for (int i = 1; i < 100; i++) {
+
+			int tens = i / 10;
+			int ones = i % 10;
+			int count = 0;
+
+			if (i >= 10) {
+				if ((tens %= 3) == 0) {
+					count++;
+				}
+			}
+
+			if (ones != 0) {
+				if ((ones %= 3) == 0) {
+					count++;
+				}
+			}
+
+			if (count > 0) {
+				System.out.print(i + " ");
+				for (int j = 0; j < count; j++) {
+					System.out.print("짝");
+				}
+				System.out.println();
+			}
+		}
 	}
 }
